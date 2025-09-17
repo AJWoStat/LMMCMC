0dcd499b0367268119ddf1f7d52ea66a1c97bbf4
Andrew Womack <ajwomack@Mac.attlocal.net>
2025-09-17 15:03:50 -0500

    corrected one more example issue and corrected bad family for custom.prior

--- R/custom.prior.R
+++ R/custom.prior.R
@@ -1,3 +1,3 @@
 custom.prior <- function(family = "Custom", log.complexity.prior) {
-  structure(list(family = "MatryoshkaDoll", hyper.parameters = log.complexity.prior), class = "prior")
+  structure(list(family = family, hyper.parameters = log.complexity.prior), class = "prior")
 }
