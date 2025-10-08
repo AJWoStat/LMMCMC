85a839c4ca17b9e0b899f62b817d9a2c31d514d8
Andrew Womack <ajwomack@Andrew-Womacks-Mac-mini.local>
2025-10-07 22:00:57 -0500

    modified cubic finder to use companion matrix method

--- src/cubic_root_finder/cubic_root_finder.c
+++ src/cubic_root_finder/cubic_root_finder.c
@@ -1,70 +1,53 @@
 #include "cubic_root_finder.h"
 
-//NOTE: RETURNS ALL REAL ROOTS, DOES NOT DEAL WITH COMPLEX ROOTS.
-double * cubic_root_finder(double par[4], int * out_length){
+struct cubic_root_finder_struct crfs = {
+  1.00, 1.00, 1.00, 1.00,
+  'N', 'N',
+  3, 
+  0.00, 1.00, 0.00, 
+  0.00, 0.00, 1.00, 
+  1.00, 1.00, 1.00, 
+  3,
+  0.00, 0.00, 0.00, 
+  0.00, 0.00, 0.00, 
+  0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 
+  0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 
+  3, 3,
+  0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 
+  6,
+  0
+  };
+
+void cubic_root_finder(double * par, double * output){
+  /*
+   * structure of the output is a length six double array 
+   * the 0-2 elements are the real parts of the solutions
+   * the 3-5 elements are the corresponding imaginary parts
+   */
+  
   //get coefs
-  double a = par[3]; //must be non-zero, but goes unchecked
-  double b = par[2];
-  double c = par[1];
-  double d = par[0]; //must be non-zero, but goes unchecked
-  //should really do some kind of a catch here
-  //if a is 0, then go to a quadratic solver
-  //if a is not 0 and d/a is 0, get 0 as a root and go to a quadratic solver
-  //these would all have to account for the sizes of other coefficients
-  //not going to code this up now, it should not be able to happen in the cases
-  //that are needed for LMMCMC
-  //but it might be worth either confirming or testing with edge cases of R^2
+  crfs.a = par[3]; //must be non-zero, but goes unchecked
+  crfs.b = par[2];
+  crfs.c = par[1];
+  crfs.d = par[0]; 
+  crfs.A[0] = 0.00; crfs.A[1] = 1.00; crfs.A[2] = 0.00;
+  crfs.A[3] = 0.00; crfs.A[4] = 0.00; crfs.A[5] = 1.00;
+  crfs.A[6] = -crfs.d/crfs.a; crfs.A[7] = -crfs.c/crfs.a; crfs.A[8] = -crfs.b/crfs.a;
   
+  F77_NAME(dgeev)(&crfs.JOBVL, &crfs.JOBVR, 
+               &crfs.N, crfs.A, &crfs.LDA,
+               crfs.WR, crfs.WI, 
+               crfs.VL, &crfs.LDVL, 
+               crfs.VR, &crfs.LDVR,
+               crfs.WORK, &crfs.LWORK,
+               &crfs.INFO
+                    FCONE FCONE
+  );
   
-  //depressed cubic
-  double root_adjust = b/(3.00*a);
-  double p_1 = c/a;
-  double p_2 = 3.00*root_adjust*root_adjust;
-  double p = p_1-p_2;
-  double q_1 = 2.00*root_adjust*root_adjust*root_adjust;
-  double q_2 = root_adjust*p_1;
-  double q_3 = d/a;
-  double q = q_1-q_2+q_3;
-  //discriminant of depressed cubic
-  double delta_1 = 4.00*p*p*p;
-  double delta_2 = 27.00*q*q;
-  double delta = -(delta_1+delta_2);
-  int out_depressed_length;
-  double * out_depressed;
-  //test number of real roots
-  if(double_is_equal(p_1, p_2) && double_is_equal(q_2-q_1, q_3)){  
-    //multiple roots scenario
-    out_depressed_length = 3;
-    out_depressed = malloc(out_depressed_length*sizeof(double));
-    //triple root
-    for(int i=0; i<3; i++) out_depressed[i] = 0.00;
-  }else if(double_is_equal(delta_1, -delta_2)==1){
-    //multiple roots scenario
-    out_depressed_length = 3;
-    out_depressed = malloc(out_depressed_length*sizeof(double));
-    //one double root and one single root
-    out_depressed[0] = 3.00*q/p;
-    out_depressed[1] = -out_depressed[0]/2.00;
-    out_depressed[2] = out_depressed[1];
-  }else if(delta<0){
-    //one distinct real root
-    out_depressed_length = 1;
-    out_depressed = malloc(out_depressed_length*sizeof(double));
-    if(double_is_equal(p_1, p_2)==1){
-      out_depressed[0] = -copysign(1.00, q)*pow(fabs(q),1.00/3.00);
-    }else if(p>0){
-      out_depressed[0] = -2.00*sqrt(p/3.00)*sinh(1.00/3.00*asinh((3.00*q)/(2.00*p)*sqrt(3.00/p)));
-    }else{
-      out_depressed[0] = -2.00*copysign(1.00, q)*sqrt(-p/3.00)*cosh(1/3.00*acosh((-3.00*fabs(q))/(2.00*p)*sqrt(-3.00/p)));
-    }
-  }else if(delta>0){
-    //three distinct real roots
-    out_depressed_length = 3;
-    out_depressed = malloc(out_depressed_length*sizeof(double));
-    for(int i=0; i<3; i++) out_depressed[i] = 2.00*sqrt(-p/3.00)*cos(1.00/3.00*acos((3.00*q)/(2.00*p)*sqrt(-3.00/p)) - 2.00*M_PI/3.00*i);
+  for(int i=0, j=3; i<3; i++, j++){
+    output[i] = crfs.WR[i];
+    output[j] = crfs.WI[j];
   }
-  (*out_length) = out_depressed_length;
-  double * out = malloc((*out_length)*sizeof(double));
-  for(int i=0; i<(*out_length); i++) out[i] = out_depressed[i] - root_adjust;
-  return(out);
+  
+  return;
 }
--- src/cubic_root_finder/cubic_root_finder.h
+++ src/cubic_root_finder/cubic_root_finder.h
@@ -4,8 +4,31 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-#include"../double_is_equal/double_is_equal.h"
+#include"R_ext/Lapack.h"
 
-double * cubic_root_finder(double * par, int * out_length);
+
+struct cubic_root_finder_struct{
+  double a;
+  double b;
+  double c;
+  double d;
+  char JOBVL, JOBVR;
+  La_INT N;
+  double A[9];
+  La_INT LDA;
+  double WR[3], WI[3];
+  double VL[9], VR[9];
+  La_INT LDVL, LDVR;
+  double WORK[18];
+  La_INT LWORK;
+  La_INT INFO;
+};
+
+void cubic_root_finder(double * par, double * output);
+/*
+ * structure of the output is a length six double array 
+ * the 0-2 elements are the real parts of the solutions
+ * the 3-5 elements are the corresponding imaginary parts
+ */
 
 #endif
