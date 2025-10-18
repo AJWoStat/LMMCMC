d85db781f1c75860f3aa996a196c6e15b661bd4d
Andrew Womack <ajwomack@Andrew-Womacks-MacBook-Air.local>
2025-10-18 16:05:30 -0500

    fixed R^2 issue when base model is not intercept

--- src/log_bf_integrator/log_bf_integrator.c
+++ src/log_bf_integrator/log_bf_integrator.c
@@ -122,7 +122,7 @@ double get_log_bf(struct model_struct * model){
   log_bf_integrator.p = (double)(model->size);
   log_bf_integrator.r = (double)(model->rank);
   log_bf_integrator.q = log_bf_integrator.eff == 1 ? log_bf_integrator.r : 1.00;
-  log_bf_integrator.Rsq = model->Rsq*data.intercept_only_model_SSE/data.base_model_SSE;
+  log_bf_integrator.Rsq = 1.00-(1.00 - model->Rsq*data.intercept_only_model_SSE)/data.base_model_SSE;
   double out = (*get_log_bf_prior_specific)(model);
   return(out);
 }
